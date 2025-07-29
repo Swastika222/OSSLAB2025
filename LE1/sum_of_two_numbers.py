b3cc47dbeef0325c55231b3b19d9c4f0d6ef117d
231B355 <231B355@juet.co.in>
2025-07-29 15:48:40 +0530

    Added solution to Lab Exercise 1

---
+++ LE1/sum_of_two_numbers.py
@@ -0,0 +1,6 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> def add_numbers(a, b):
+...     return a + b
+... num1 = int(input("Enter first number: "))
+... num2 = int(input("Enter second number: "))
